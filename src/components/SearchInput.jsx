6f0a2d5c72cf69ce124354eef54de0ab757ae0f9
Oleksandr Budik <sashabudik@gmail.com>
2023-10-18 18:55:00 +0200

    add search term to hash

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -3,14 +3,34 @@ import { GlobalContext } from '../App.js'
 import '../styles/SearchInput.scss'
 
 const SearchInput = ({ label = 'Search', ...rest }) => {
-  const [, setSearchTerm] = useContext(GlobalContext)
+  const [searchTerm, setSearchTerm] = useContext(GlobalContext)
+  console.log(window.location)
+  if (window.history.replaceState) {
+    const url =
+      window.location.protocol +
+      '//' +
+      window.location.host +
+      window.location.pathname
+    window.history.replaceState(
+      {
+        path: url,
+      },
+      '',
+      `${searchTerm ? `#${searchTerm}` : ''}`
+    )
+  }
   const handleChange = (e) => {
     setSearchTerm(e.target.value)
   }
   return (
     <div className={'search-input__wrapper'}>
       <div className="search-input__label">{label}</div>
-      <input className="input-area" onChange={handleChange} {...rest} />
+      <input
+        className="input-area"
+        value={searchTerm}
+        onChange={handleChange}
+        {...rest}
+      />
     </div>
   )
 }
